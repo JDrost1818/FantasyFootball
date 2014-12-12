586d72c8ebf65a38db4c8b2a9f19129882f52c80
Jake <Drost011@umn.edu>
2014-12-12 14:15:22 -0600

    Stuff

--- db/migrate/20141210190639_create_teams.rb
+++ db/migrate/20141210190639_create_teams.rb
@@ -1,6 +1,7 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
+      t.integer :team_id
       t.integer :user_id
       t.string :name
       t.integer :total_salary
