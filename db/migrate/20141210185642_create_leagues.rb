248b0442e6ce6201d4f4fb563346fb9efab60068
Jake <Drost011@umn.edu>
2014-12-11 18:14:17 -0600

    Updated migrations

--- db/migrate/20141210185642_create_leagues.rb
+++ db/migrate/20141210185642_create_leagues.rb
@@ -1,6 +1,7 @@
 class CreateLeagues < ActiveRecord::Migration
   def change
     create_table :leagues do |t|
+      t.integer :id
       t.string :description
       t.string :name
       t.integer :salary_cap
